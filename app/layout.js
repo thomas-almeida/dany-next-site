cc2aa73cac2fdf9583c34d08b9cc20252e7db603
thomas almeida <contato.thomasalmeidard@gmail.com>
2025-12-11 18:48:02 -0300

    MS clarity

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/next";
+import Script from 'next/script';
 
 export const metadata = {
   title: "Dany Terapia Corporal - Massagem Profissional em São Paulo",
@@ -13,6 +14,15 @@ export default function RootLayout({ children }) {
       <Analytics />
       <head>
         <meta name="google-site-verification" content="Q0zjLFrZMxnyQ0pg7qy-xqrieBim5tK-7itR74qjN0w" />
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "uk1j7a0pef");
+          `}
+        </Script>
       </head>
       <body
         className={`antialiased`}
