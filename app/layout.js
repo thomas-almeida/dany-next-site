67b4df26741506fc96b82159973bda1afd73437a
thomas-almeida <contato.thomasalmeidard@gmail.com>
2026-03-16 14:32:39 -0300

    Clarity

--- app/layout.js
+++ app/layout.js
@@ -100,7 +100,7 @@ export default function RootLayout({ children }) {
               c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
               t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
               y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "uk1j7a0pef");
+            })(window, document, "clarity", "script", "vwrpgjmkek");
           `}
         </Script>
       </head>
